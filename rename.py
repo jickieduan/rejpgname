c2fc7ace168d35f2b0edd4c6be50c86c86a852ad
jickieduan <jickieduan@users.noreply.github.com>
2017-10-28 10:38:52 +0800

    rename overed

--- rename.py
+++ rename.py
@@ -11,6 +11,8 @@ img =None
 i=0;
 s = None
 dirs = None
+li_name=["_ddd","_jjj"]
+is_rename=0
 
 class Application(Frame):
         def __init__(self, master=None):
@@ -20,64 +22,90 @@ class Application(Frame):
 
         def createWidgets(self):
             self.path = StringVar()
-            self.path.set("e:/duorou1")
+            self.nname = StringVar()
+            self.path.set("")
             self.L_path = Label(self,text = "多肉位置：",font =(16))
-            self.L_path.pack(padx=5,pady=10,side=LEFT)
+            self.L_path.grid(row=1,column=1)
             self.E_path = Entry(self,textvariable = self.path,font =(16))
-            self.E_path.pack(padx=5,pady=20,side=LEFT)
+            self.E_path.grid(row=1,column=2)
             self.B_path = Button(self,text = "路径选择",font =(16),command = self.selectPath)
-            self.B_path.pack(padx=5,pady=20,side=LEFT)
+            self.B_path.grid(row=1,column=3)
             self.L_name = Label(self,text = "修改为：",font = (16))
-            self.L_name.pack(padx=5,pady=50,side=BOTTOM)
-            self.nameInput = Entry(self, font =("楷体",16))
-            self.nameInput.pack(padx=5,pady=500,side=LEFT)
+            self.L_name.grid(row=2,column=1)
+            self.nameInput = Entry(self, textvariable = self.nname,font =(16))
+            self.nameInput.grid(row=2,column=2)
             self.quitButton = Button(self, text="确定", font=(16), command=self.rename)
-            self.quitButton.pack(padx=5,pady=50,side=LEFT)
+            self.quitButton.grid(row=2,column=3)
         def showImg(self):
             global img
             load = Image.open(s)
             render= ImageTk.PhotoImage(load)
             img = Label(self,image=render)
             img.image = render
-            img.pack()
+            img.grid(row=3,columnspan=10)
             return img
         def rename(self):
             global i
             global s
             global img
+            global li_name
+            global is_rename
+            if dirs == None:
+                    showwarning("提示","请先选择文件夹！")
+                    is_rename = 1
             n=self.nameInput.get()
-            if i<len(dirs)-3:
-                print i,len(dirs)
-                os.rename(rootdir+os.sep+dirs[i],rootdir+os.sep+n+".jpg")
-                os.rename(rootdir+os.sep+dirs[i+1],rootdir+os.sep+n+"(1)"+".jpg")
-                os.rename(rootdir+os.sep+dirs[i+2],rootdir+os.sep+n+"(2)"+".jpg")
+            _n=n+".jpg"
+            for  named in li_name:
+                    if named == _n:
+                            showwarning("警告","这个名字已经存在啦！")
+                            is_rename = 1
+                            break
+                
+            if is_rename == 0 :
+                os.rename(self.E_path.get()+os.sep+dirs[i],self.E_path.get()+os.sep+n+".jpg")
+                os.rename(self.E_path.get()+os.sep+dirs[i+1],self.E_path.get()+os.sep+n+"(1)"+".jpg")
+                os.rename(self.E_path.get()+os.sep+dirs[i+2],self.E_path.get()+os.sep+n+"(2)"+".jpg")
+                li_name.append(n+".jpg")
+                li_name.append(n+"(1)"+".jpg")
+                li_name.append(n+"(2)"+".jpg")
                 i=i+3
-                s=rootdir+os.sep+dirs[i]
-                load = Image.open(s)
-                render= ImageTk.PhotoImage(load)
-                img.configure(image = render)
-                img.image = render
-            else:
-                showwarning("open","修改已全部完成！")
+                if (i>=len(dirs)):
+                        is_rename = 1
+                        showwarning("提示","修改已全部完成！")
+                else:
+                        s=self.E_path.get()+os.sep+dirs[i]
+                        load = Image.open(s)
+                        render= ImageTk.PhotoImage(load)
+                        img.configure(image = render)
+                        img.image = render
+                        self.nname.set("")
+            elif is_rename == 1:
+                showwarning("提示","修改已全部完成！")
         def  selectPath(self):
             path_ = tkFileDialog.askdirectory()
             global dirs
             global s
+            global li_name
             pattern = re.compile(".*\.(jpg|JPG)")
             is_show = 1
             self.path.set(path_)
             dirs = os.listdir(path_)
-            s=path_+os.sep+dirs[i]
+            if(len(dirs)==0):
+                    showwarning("警告","您选择了一个空文件夹。。。")
+                    is_show = 0
+            if is_show ==1:
+                    s=path_+os.sep+dirs[i]
             for file in dirs:
                 if not pattern.match(file):
-                    print file
-                    showwarning("open","请选择全为图片的文件夹！")
+                    showwarning("提示","请选择全为图片的文件夹！")
                     is_show = 0
                     break
-            if is_show == 1 and len(dirs)//3 !=0:
-                showwarning("open","警告：您所选择的图片个数不为3的倍数\n重命名后文件名可能发生错乱\n请修改文件夹后重试")
+            if is_show == 1 and len(dirs)%3 !=0:
+                showwarning("警告","警告：您所选择的图片个数不为3的倍数\n重命名后文件名可能发生错乱\n请修改文件夹后重试")
                 is_show = 0
-            if is_show == 1:    
+            if is_show == 1:
+                for file in dirs:
+                        li_name.append(str(file))
                 self.showImg()
             
 app =Application()
